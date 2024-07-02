74012dc3b84c2b8c97066ef2e813d6893a8700ce
Joel <joel.viana@audora.com.br>
2024-07-02 13:53:30 -0300

    feat:implementing customized messages, course title validation.

--- src/main/java/com/joel/learn/course/domain/services/impl/CourseServiceImpl.java
+++ src/main/java/com/joel/learn/course/domain/services/impl/CourseServiceImpl.java
@@ -5,6 +5,7 @@ import com.joel.learn.course.domain.dtos.CourseDTO;
 import com.joel.learn.course.domain.dtos.CourseRequestDTO;
 import com.joel.learn.course.domain.dtos.PurchaseEventDTO;
 import com.joel.learn.course.domain.excptions.CourseNotFoundException;
+import com.joel.learn.course.domain.excptions.TitleAlreadyExistsException;
 import com.joel.learn.course.domain.models.Course;
 import com.joel.learn.course.domain.repositories.CourseRepository;
 import com.joel.learn.course.domain.services.CourseService;
@@ -24,6 +25,7 @@ import java.util.UUID;
 @Service
 public class CourseServiceImpl implements CourseService {
 
+    public static final String THERE_IS_ALREADY_A_BOOK_WITH_THIS_TITLE = "Book cannot be saved because there is already a book with that title";
     private final CourseRepository courseRepository;
     private final CourseConverter courseConverter;
     private final PurchaseCommandPublisher purchaseCommandPublisher;
@@ -53,6 +55,7 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public CourseDTO save(CourseRequestDTO courseRequestDTO) {
         Course course = courseConverter.toEntity(courseRequestDTO);
+        validateTitle(courseRequestDTO);
 
         log.info("Saving course from CourseId : {}", course.getCourseId());
         return courseConverter.toDTO(courseRepository.save(course));
@@ -80,5 +83,11 @@ public class CourseServiceImpl implements CourseService {
     public Course optionalCourse(UUID courseId) {
         return courseRepository.findById(courseId)
                 .orElseThrow(() -> new CourseNotFoundException(courseId));
+    }
+
+    private void validateTitle(CourseRequestDTO courseRequestDTO) {
+        if (courseRepository.existsByTitle(courseRequestDTO.getTitle())) {
+            throw new TitleAlreadyExistsException(THERE_IS_ALREADY_A_BOOK_WITH_THIS_TITLE);
+        }
     }
 }
--- src/main/resources/db/testdb/afterMigrate.sql
+++ src/main/resources/db/testdb/afterMigrate.sql
@@ -3,3 +3,13 @@
 DELETE FROM COURSE;
 DELETE FROM USERS;
 DELETE FROM COURSES_USERS;
+
+INSERT INTO course (course_id, title, subtitle, price, creation_date)
+VALUES (
+    '0a657c35-e0ed-44e0-b25c-3afbeadd643e',
+    'Java Developer',
+    'Java advanced  for developers',
+     99.99,
+    '2024-07-01 10:30:00'
+);
+
